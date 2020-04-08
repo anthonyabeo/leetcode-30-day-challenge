af0c682a50cb3b32cfec567be8c1210266ee9d04
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-04-08 19:20:21 +0000

    add annotations for the middle of linked list problem

--- middle_linked_list.cpp
+++ middle_linked_list.cpp
@@ -7,12 +7,17 @@
  * };
  */
 
+/**
+ * The tortoise and hare (slow and fast pointer) technique works because starting from
+ * the head node, if the hare runs twice as fast as the tortoise, then when the hare gets 
+ * to the end of the list, the tortoise would have reached the middle.
+*/
  struct ListNode {
       int val;
       ListNode *next;
       ListNode(int x) : val(x), next(NULL) {}
  };
- 
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
